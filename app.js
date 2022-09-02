f3f8d1731ee6f508a1d9a248ed12acd6db2d2a91
Andrew Kim <akim48415@gmail.com>
2022-09-01 23:44:44 -0400

    cleaned up, need to add mobile compatibility

--- app.js
+++ app.js
@@ -10,65 +10,6 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-const highlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const educationMenu = document.querySelector('#education-page')
-    const skillsMenu = document.querySelector('#skills-page')
-    const experienceMenu = document.querySelector('#experience-page')
-    const projectsMenu = document.querySelector('#projects-page')
-    const awardsMenu = document.querySelector('#awards-page')
-    const moreMenu = document.querySelector('#more-page')
-    let scrollPos = window.scrollY
-    console.log(scrollPos)
-
-    if(window.innerWidth > 960 && scrollPos < 800) {
-        removeHighlightMenu()
-        homeMenu.classList.add('highlight')
-        return
-    }else if(window.innerWidth > 960 && scrollPos < 1300) {
-        removeHighlightMenu()
-        educationMenu.classList.add('highlight')
-        return
-    }else if(window.innerWidth > 960 && scrollPos < 1700) {
-        removeHighlightMenu()
-        skillsMenu.classList.add('highlight')
-        return
-    }else if(window.innerWidth > 960 && scrollPos < 2400) {
-        removeHighlightMenu()
-        skillsMenu.classList.add('highlight')
-        return
-    }
-
-    if((elem && window.innerWidth < 960 && scrollPos < 800) || elem) {
-        elem.classList.remove('highlight')
-    }
-};
-
-window.addEventListener('scroll', highlightMenu)
-window.addEventListener('click', highlightMenu)
-
-const removeHighlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const educationMenu = document.querySelector('#education-page')
-    const skillsMenu = document.querySelector('#skills-page')
-    const experienceMenu = document.querySelector('#experience-page')
-    const projectsMenu = document.querySelector('#projects-page')
-    const awardsMenu = document.querySelector('#awards-page')
-    const moreMenu = document.querySelector('#more-page')
-
-    homeMenu.classList.remove('highlight')
-    educationMenu.classList.remove('highlight')
-    skillsMenu.classList.remove('highlight')
-    experienceMenu.classList.remove('highlight')
-    projectsMenu.classList.remove('highlight')
-    awardsMenu.classList.remove('highlight')
-    moreMenu.classList.remove('highlight')
-};
-
-
-
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active')
     if(window.innerWidth <= 768 && menuBars) {
